70d5f2c49fd6183ed562967e8522d9add908b568
Weskley Bezerra <weskleymb@hotmail.com>
2019-07-02 21:25:13 -0300

    commit inicial

---
+++ js/exemplo1.js
@@ -0,0 +1,17 @@
+//solicita valores
+let valor1 = prompt('Digite o valor 1');
+let valor2 = prompt('Digite o valor 2');
+
+//converte valores em inteiro
+valor1 = parseInt(valor1);
+valor2 = parseInt(valor2);
+
+//realiza operações
+let soma = valor1 + valor2;
+let subtracao = valor1 - valor2;
+let multiplicacao = valor1 * valor2;
+let divisao = valor1 / valor2;
+let potencia = valor1 ** valor2;
+let resto = valor1 % valor2;
+
+alert('O resultado é ' + divisao);
